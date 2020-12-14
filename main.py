26a3c4a540371d2815212a7c10eb1314f84243bd
snapp <37773437+VladimirMeshcherekov@users.noreply.github.com>
2020-12-14 16:56:28 +0300

    Upd 1.1

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from typing import Final
 import math
 
@@ -26,4 +23,4 @@ for z in range(-2, 13):
 
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# t.me/snappy
