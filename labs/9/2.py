cf35638804d91eb7f6d1012d46ac2322903f0ab8
snapp <37773437+VladimirMeshcherekov@users.noreply.github.com>
2021-01-14 01:09:04 +0300

    Update 2.py
    1.1 upd hotfix

--- labs/9/2.py
+++ labs/9/2.py
@@ -1,9 +1,19 @@
-n = 2
+
 su = 0
-a = [[0] * n for i in range(n)]
+
+# в первой строке ввода идёт количество строк массива
+n = int(input())
+a = []
+for i in range(n):
+    row = input().split()
+    for i in range(len(row)):
+        row[i] = int(row[i])
+    a.append(row)
+
+
+
 for i in range(n):
     for j in range(n):
-        a[i][j] = int(input()) #ввод ебанутый, но решение лучше я пока не придумал
         if (i + j == n-1):
             su = su + a[i][j]
 
@@ -11,7 +21,6 @@ for row in a:
     print(' '.join([str(elem) for elem in row]))
 print(su) #побочная диагональ
 
-print(a[0][1])
 
 for j in range(n):
     for i in range(n):
